61cb727d146bde76dba6ec197f05238fc8172c1a
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 20:39:01 +0800

    Update json-2.php

--- json-2.php
+++ json-2.php
@@ -2,12 +2,14 @@
 $json = file_get_contents('http://dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com/json-activity/json-1.php');
 
 $data = json_decode($json,true);
-
 $list = $data['articles'][1];
 
-//var_dump($data);
-//echo "<pre>";
-
-print_r($list);
+foreach($list as $value){
+  echo $value['article_id'];
+  echo $value['article_title'];
+  echo $value['description'];
+  echo $value['author'];
+  echo $value['date_created'];
+}
 
 ?>
