f0adc1a0f1dcf23b8230d75b14ea20ed038239d4
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 20:20:54 +0800

    Update json-2.php

--- json-2.php
+++ json-2.php
@@ -1,5 +1,5 @@
 <?php
-$json = file_get_contents('http://dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com/json-activity/json-1.php');
+$json = file_get_contents('http://dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com/json-activity/index.php');
 
 $data = json_decode($json,true);
 
