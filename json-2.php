b87a5e9a58ed2bc952512554236262f0852419b4
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-28 11:18:36 +0800

    Update json-2.php

--- json-2.php
+++ json-2.php
@@ -5,14 +5,9 @@ $data = json_decode($json,true);
 $list = $data['articles'];
 
 foreach($list as $value){
-?>
-<ul>
-  <li> <?php echo $value['article_id'];?> </li>
-  <li> <?php echo $value['article_title'];?> </li>
-  <li> <?php echo $value['description'];?> </li>
-  <li> <?php echo $value['author'];?> </li>
-  <li> <?php echo $value['date_created'];?> </li>
-</ul>
-<?php
-}
+  echo $value['article_id'];
+  echo $value['article_title'];
+  echo $value['description'];
+  echo $value['author'];
+  echo $value['date_created'];
 ?>
