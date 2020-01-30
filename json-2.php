07346b053eca187a0cef170f2b601e61fb5fa13b
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-30 17:38:40 +0800

    Update json-2.php

--- json-2.php
+++ json-2.php
@@ -1,32 +1,3 @@
-<style>
-
-    .content{
-        display: flex;
-        justify-content: center;
-    }
-	table{
-		border: 1px solid black;
-		width: 100%;
-		text-align: center;
-		
-	}
-	th, td{
-		border: 1px solid #ddd;
-		padding: 10px;
-
-	}
-    tr:nth-child(even) {background-color: pink;}
-	a {
-		color: #66fcf1;
-	}
-    .head{
-		background-color: #45a29e;
-		color: #1f2833;
-		font-weight: bold;
-	}
-</style>
-
-
 <?php
 $json = file_get_contents('https://json-activity.herokuapp.com/json-1.php');
 
@@ -34,19 +5,12 @@ $data = json_decode($json,true);
 $list = $data['articles'];
 //$list = $data['pets'][1];
 
-
-//var_dump($data);
-//echo "<pre>";
-
-//print_r($list);
 ?>
-<div class="content">
+
 <h1>ARTICLES</h1>
-</div>
-<div class="content">
+
 <table border="1px">
     <tr class="head">
-        <td>Article ID</td>
         <td>Article Title</td>
         <td>Content</td>
         <td>Author</td>
@@ -56,7 +20,6 @@ $list = $data['articles'];
 foreach($list as $value){
     ?>
     <tr>
-        <td><?php echo $value['article_id'];?></td>
         <td><?php echo $value['article_title'];?></td>
         <td><?php echo $value['description'];?></td>
         <td><?php echo $value['author'];?></td>
@@ -66,4 +29,3 @@ foreach($list as $value){
 }
 ?>
 </table>
-</div>
